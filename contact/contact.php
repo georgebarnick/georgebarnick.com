1506556378cc81ba6ba7b39e36b4379c0eb9be70
George Barnick <georgebarnick@gmail.com>
2018-11-22 22:55:35 +0000

    Fix CAPTCHA triggering/verification method

--- contact/contact.php
+++ contact/contact.php
@@ -9,27 +9,27 @@ $body_message .= 'E-mail: ' . $field_email . "\n";
 $body_message .= 'Message: ' . $field_message;
 $headers = "From: " . $field_email . "\r\n";
 $headers .= "Reply-To: " . $field_email . "\r\n";
-if ( $field_email != null && $field_message != null ) {
-	$mail_status = mail( $mail_to, $subject, $body_message, $headers );
-}
-if ( $mail_status && $field_email != null && $field_message != null ) {
-	if ( isset( $_POST['g-recaptcha-response'] ) && !empty( $_POST['g-recaptcha-response'] ) ) {
+if ( isset( $_POST['g-recaptcha-response'] ) && !empty( $_POST['g-recaptcha-response'] ) ) {
+	if ( $field_email != null && $field_message != null ) {
+		$mail_status = mail( $mail_to, $subject, $body_message, $headers );
+	}
+	if ( $mail_status && $field_email != null && $field_message != null ) {
 		require_once( 'secret.php' );
 		$verifyResponse = file_get_contents( 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response'] );
 		$responseData = json_decode( $verifyResponse );
-		if( $responseData->success ) { ?>
+		if ( $responseData->success ) { ?>
 			<script language="javascript" type="text/javascript">
 				window.location = '/contact/?result=success';
 			</script>
-		<?php } else { ?>
-			<script language="javascript" type="text/javascript">
-				window.location = '/contact/?result=verificationfail&name=<?php echo $field_name; ?>&email=<?php echo $field_email; ?>&message=<?php echo $field_message ?>#Contact';
-			</script>
 		<?php }
+	} else { ?>
+	    <script language="javascript" type="text/javascript">
+			window.location = '/contact/?result=error&name=<?php echo $field_name; ?>&email=<?php echo $field_email; ?>&message=<?php echo $field_message ?>#Contact';
+	    </script>
+	<?php
 	}
 } else { ?>
-    <script language="javascript" type="text/javascript">
-		window.location = '/contact/?result=error&name=<?php echo $field_name; ?>&email=<?php echo $field_email; ?>&message=<?php echo $field_message ?>#Contact';
-    </script>
-<?php
-}?>
+	<script language="javascript" type="text/javascript">
+		window.location = '/contact/?result=verificationfail&name=<?php echo $field_name; ?>&email=<?php echo $field_email; ?>&message=<?php echo $field_message ?>#Contact';
+	</script><?php
+} ?>
